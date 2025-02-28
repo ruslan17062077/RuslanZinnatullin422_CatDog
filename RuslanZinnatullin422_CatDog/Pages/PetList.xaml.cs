e8b458426e7de3bc3bdb2091fc17187683609f3b
ruslan17062077 <ybavpr61@gmail.com>
2025-02-28 14:12:43 +0300

    Add Authorization

---
+++ RuslanZinnatullin422_CatDog/Pages/PetList.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RuslanZinnatullin422_CatDog.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PetList.xaml
+    /// </summary>
+    public partial class PetList : Page
+    {
+        public PetList()
+        {
+            InitializeComponent();
+        }
+
+        private void poisk_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
